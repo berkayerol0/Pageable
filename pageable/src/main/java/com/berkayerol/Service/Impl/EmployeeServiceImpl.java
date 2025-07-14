aeabbdab9a861c10d5b917e8b33a9975492c31c4
Berkay <berkay.erol.354@hotmail.com>
2025-07-14 17:57:18 +0300

    fix(security): Fortify - Resolved Poor Logging Practice in EmployeeServiceImpl.java

--- pageable/src/main/java/com/berkayerol/Service/Impl/EmployeeServiceImpl.java
+++ pageable/src/main/java/com/berkayerol/Service/Impl/EmployeeServiceImpl.java
@@ -5,6 +5,8 @@ import com.berkayerol.Dto.DtoEmployee;
 import com.berkayerol.Model.Employee;
 import com.berkayerol.Repository.EmployeeRepository;
 import com.berkayerol.Service.IEmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -17,17 +19,21 @@ import java.util.List;
 @Service
 public class EmployeeServiceImpl implements IEmployeeService {
 
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+
     @Autowired
     private EmployeeRepository employeeRepository;
 
     @Override
     public Page<Employee> findAllPageable(Pageable pageable) {
+        logger.info("Fetching employees with pageable: {}", pageable); // Added logging
         Page<Employee> page = employeeRepository.findAllPageable(pageable);
         return page;
     }
 
     @Override
     public List<DtoEmployee> toDTOList(List<Employee> employeeList) {
+        logger.info("Converting Employee list to DTO list. Size: {}", employeeList.size()); // Added logging
         List<DtoEmployee> dtoList = new ArrayList<>();
 
         for(Employee employee: employeeList) {
